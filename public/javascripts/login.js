2239ef6aeadffeb2b8338e4ee0e5a12640719a70
lyzhang <2095438498@qq.com>
2020-01-19 09:50:08 +0800

    聊天界面编写，引用相关模块。

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,11 +1,10 @@
-let bbtn = document.getElementById('login');
-console.log(bbtn);
+const http = require('../ajax');
+// const http = require('http');
 window.onload = function () {
     document.getElementById('login').onclick = function () {
-        console.log('点击登录');
         let user = 'liyan';
         let pwd = '123456';
-        window.ajax({
+        http.ajax({
             url: "/api/login?user='" + user + "'&pwd='" + pwd + "'",
             method: "GET",
             dataType: "JSON",
@@ -24,53 +23,5 @@ window.onload = function () {
                 console.log(res);
             }
         });
-    };
-};
-window.ajax = function ({url,method,dataType,data,headers,success,error}) {
-    function createXHR() {
-        if (typeof XMLHttpRequest !== 'undefined') {
-            return new XMLHttpRequest();
-        } else if (typeof ActiveXObject !== 'undefined') {
-            if (typeof arguments.callee.activeXString !== 'string') {
-                let versions = ['MSXML2.XMLHttp.6.0', 'MSXML2.XMLHttp.3.0', 'MSXML2.XMLHttp'], i, len;
-                for (i = 0, len = versions.length; i < len; i++) {
-                    try {
-                        new ActiveXObject(versions[i]);
-                        arguments.callee.activeXString = versions[i];
-                        break;
-                    } catch (e) {
-                        console.log('不存在！！')
-                    }
-                }
-            }
-            return new ActiveXObject(arguments.callee.activeXString);
-        }
-    }
-    const xhr = new createXHR();
-    if (method.toLowerCase() !== 'get') {
-        url = url + "?";
-        for (let key in data) {
-            let value = data[key];
-            url += key + "=" + value + "&";
-        }
-        data = null;
-    }
-    xhr.open(method,url);
-    /*要成功发送setRequestHeader()头部信息，必须在open()之后，send()之前调用*/
-    for (let key in headers) {
-        let value = headers[key];
-        xhr.setRequestHeader(key, value);
-    }
-    xhr.send(data);
-    xhr.onload = function () {
-        if (xhr.readyState === 4) {
-            if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
-                success.call(undefined, JSON.parse(xhr.responseText));
-                document.writeln(xhr.getResponseHeader('Content-Type') + ":::::");
-                document.writeln(xhr.getAllResponseHeaders());
-            } else {
-                error.call(undefined, xhr);
-            }
-        }
     };
 };
--- service/ajax.js
+++ service/ajax.js
@@ -1,23 +1,4 @@
 function ajax({url,method,dataType,data,headers,success,error}) {
-    function createXHR() {
-        if (typeof XMLHttpRequest !== 'undefined') {
-            return new XMLHttpRequest();
-        } else if (typeof ActiveXObject !== 'undefined') {
-            if (typeof arguments.callee.activeXString !== 'string') {
-                let versions = ['MSXML2.XMLHttp.6.0', 'MSXML2.XMLHttp.3.0', 'MSXML2.XMLHttp'], i, len;
-                for (i = 0, len = versions.length; i < len; i++) {
-                    try {
-                        new ActiveXObject(versions[i]);
-                        arguments.callee.activeXString = versions[i];
-                        break;
-                    } catch (e) {
-                        console.log('不存在！！')
-                    }
-                }
-            }
-            return new ActiveXObject(arguments.callee.activeXString);
-        }
-    }
     const xhr = new createXHR();
     if (method.toLowerCase() !== 'get') {
         url = url + "?";
@@ -38,14 +19,31 @@ function ajax({url,method,dataType,data,headers,success,error}) {
         if (xhr.readyState === 4) {
             if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
                 success.call(undefined, JSON.parse(xhr.responseText));
-                document.writeln(xhr.getResponseHeader('Content-Type') + ":::::");
-                document.writeln(xhr.getAllResponseHeaders());
+                // document.writeln(xhr.getResponseHeader('Content-Type') + ":::::");
+                // document.writeln(xhr.getAllResponseHeaders());
             } else {
                 error.call(undefined, xhr);
             }
         }
     };
 };
-module.exports = {
-    ajax
+function createXHR() {
+    if (typeof XMLHttpRequest !== 'undefined') {
+        return new XMLHttpRequest();
+    } else if (typeof ActiveXObject !== 'undefined') {
+        if (typeof arguments.callee.activeXString !== 'string') {
+            let versions = ['MSXML2.XMLHttp.6.0', 'MSXML2.XMLHttp.3.0', 'MSXML2.XMLHttp'], i, len;
+            for (i = 0, len = versions.length; i < len; i++) {
+                try {
+                    new ActiveXObject(versions[i]);
+                    arguments.callee.activeXString = versions[i];
+                    break;
+                } catch (e) {
+                    console.log('不存在！！')
+                }
+            }
+        }
+        return new ActiveXObject(arguments.callee.activeXString);
+    }
 };
+module.exports = ajax;
